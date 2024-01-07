0756c9594ec72b74787ebd0b344fa1a1ccafd948
prashbala27 <136168028+prashbala27@users.noreply.github.com>
2024-01-07 07:08:53 +0000

    Updated something

--- pexpense_load.py
+++ pexpense_load.py
@@ -17,18 +17,24 @@ def create_connection():
 
 # Function to insert data into the PostgreSQL database
 def insert_data(connection, cursor, data):
-    query = sql.SQL("INSERT INTO persfinance.Daily_Expenses (date, category, credit_debit, amount, description) VALUES (%s, %s, %s, %s, %s)")
-    cursor.execute(query, (data["Date"], data["Category"], data["Credit/Debit"], data["Amount"], data["Description"]))
+    query = sql.SQL("INSERT INTO persfinance.Daily_Expenses (date, category, subcategory, credit_debit, amount, description, bank_details) VALUES (%s, %s, %s, %s, %s, %s, %s)")
+    cursor.execute(query, (data["Date"], data["Category"], data["Subcategory"], data["Credit/Debit"], data["Amount"], data["Description"], data["Bank Details"]))
     connection.commit()
+
 def main():
     st.title("Expense Tracker")
 
     # Define categories and credit/debit options
-    categories = ["Food", "Utilities", "Transportation", "Entertainment", "Miscellaneous"]
+    categories = ["Food", "Utilities", "Transportation", "Entertainment", "Miscellaneous","Return Payment","Investment"]
+    subcategories = ["Investment-Gold", "Investment-MutualFund", "Investment-TermInsurancePayment", 
+                     "Food-Family", "Food-Friends","Misc-OnlinePurchase","Utils-2Wheeeler",
+                     "Utils-4Wheeler","Trasnport-TrainTicket","Trasnport-Flight","Misc-Trip",
+                     "Utils-EB","Utils-Mobile","Utils-Broadband","Enter-Netflix","Enter-Xbox",]
     credit_debit_options = ["Credit", "Debit"]
+    bank_details_options = ["HDFC-9325", "HDFC-1148", "SBI-4398", "EQSM-1764"]
 
     # Create two columns for horizontal layout
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
 
     # Get user input in the first column
     with col1:
@@ -37,27 +43,34 @@ def main():
 
     # Get user input in the second column
     with col2:
+        subcategory = st.selectbox("Subcategory", subcategories)
         credit_debit = st.selectbox("Credit/Debit", credit_debit_options)
         amount = st.number_input("Amount", value=0.0, step=1.0)
-        description = st.text_area("Description", "")
 
-    # Add a "Submit" button
+    with col3:
+        description = st.text_area("Description", "")
+        bank_details = st.selectbox("Bank Details", bank_details_options)
+
     if st.button("Submit"):
         entered_data = {
             "Date": date,
             "Category": category,
+            "Subcategory": subcategory,
             "Credit/Debit": credit_debit,
             "Amount": amount,
-            "Description": description
+            "Description": description,
+            "Bank Details": bank_details
         }
 
         # Display the entered information
         st.write("## Entered Information:")
         st.write(f"**Date:** {entered_data['Date']}")
         st.write(f"**Category:** {entered_data['Category']}")
+        st.write(f"**Subcategory:** {entered_data['Subcategory']}")
         st.write(f"**Credit/Debit:** {entered_data['Credit/Debit']}")
         st.write(f"**Amount:** {entered_data['Amount']}")
         st.write(f"**Description:** {entered_data['Description']}")
+        st.write(f"**Bank Details:** {entered_data['Bank Details']}")
 
         # Write data to PostgreSQL database
         connection, cursor = create_connection()
